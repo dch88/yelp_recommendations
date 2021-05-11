597db939500a72ae19fdc471d4dba79925ce0b57
dch88 <dch88@georgetown.edu>
2021-05-10 22:45:46 -0400

    Navigable App

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
+
+  var users = [
+    {"id": 1, "name": "Jane McConnel", "email": "jane@example.com"},
+    {"id": 2, "name": "Earl Jones", "email": "earl@yahoo.com"},
+    {"id": 3, "name": "Sammy Student", "email": "sammy@myschool.edu"},
+  ] // just some dummy data
+  res.send(users);
+
 });
 
 module.exports = router;
