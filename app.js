6834c9c82e969b2b410e54724c721ccfe5d97593
dch88 <dch88@georgetown.edu>
2021-05-11 07:55:36 -0400

    Data processing

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); //go look in .env file for some environment variables
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,12 +8,14 @@ var logger = require('morgan');
 var expressLayouts = require('express-ejs-layouts'); 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var stocksRouter = require('./routes/stocks')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 app.use(expressLayouts); 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/stocks', stocksRouter) // around line 29
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
