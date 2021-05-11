7bd3e07dc2237f449840a07542e49aadb78b33b4
dch88 <dch88@georgetown.edu>
2021-05-10 22:52:06 -0400

    Setup another user route

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,15 @@ router.get('/', function(req, res, next) {
     {"id": 3, "name": "Sammy Student", "email": "sammy@myschool.edu"},
   ] // just some dummy data
   res.send(users);
+});
+
+router.get('/:id', function(req, res, next) {
+  var userId = req.params.id;
+  console.log("USER ID:", userId)
+
+  var user = {"id": userId, "name": "Example User", "email": "example@example.com"} // just some dummy data
+  res.send(user);
+
 
 });
 
